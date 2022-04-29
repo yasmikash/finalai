1d19b62b3b878cc6d8e31f9ea553a479ed1deb48
yasmikash <yashtricksz@gmail.com>
2022-04-29 22:48:45 +0530

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+import json
+import numpy as np
+import pickle
+from flask import Flask, request, jsonify
+
+# define a flask app
+app = Flask(__name__)
+
+@app.route('/status',methods=['POST'])
+def status():
+    model = pickle.load(open('models/final_ai_model.pkl', 'rb'))
+    # get the data from the POST request.
+    data = request.get_json(force=True)
+    # make prediction using model loaded from disk as per the data.
+    data_predict = np.array(data['data']).reshape((1, -1))
+    prediction = model.predict(data_predict)
+
+    # prediction probability
+    prediction_probability = np.max(model.predict_proba(data_predict), axis=1)
+
+    # take the first value of prediction
+    final_status = prediction[0]
+    return jsonify(status=json.dumps(final_status.astype(float)), probability=json.dumps(prediction_probability[0].astype(float)))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
